f8f1eb2b3ea13a982b1e23d6dc168199c4ca5c8b
sarangsalunkhe <sarangsalunkhe2@gmail.com>
2024-03-09 10:07:46 +0530

    Add project files.

---
+++ ConsoleApp2/NewFolder1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.NewFolder1
+{
+    internal class Class1
+    {
+        private string[] name { get; set; }
+
+       
+
+        public void TwoNoSum()
+        {
+            int a = 10;
+            int b = 10;
+
+            int c = a + b;
+
+            Console.WriteLine(c);
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ConsoleApp2.NewFolder1;
+using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 c = new Class1();
+
+            c.TwoNoSum();
+            Console.ReadLine();
+
+
+        }
+    }
+}
